62899379bdc5d15b2ee7a202c07c2e8c5ad6422a
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-02-09 00:46:47 +0600

    array multielement odd number sum

--- jsarray.js
+++ jsarray.js
@@ -6,7 +6,25 @@ function getSumNumbers(numbers){
     let element = numbers[index];
     sum = sum + element;
   }
+  return(sum);
   // console.log(sum)
 }
-let numbers  = [60, 34, 12, 26, 10];
-getSumNumbers(numbers)
+// let numbers  = [60, 34, 12, 26, 10];
+
+// array multielement odd numbers summtion------------
+function getOddNumbersAnArray(numbers){
+  let oddNumbers = [];
+  for(let i = 0; i < numbers.length; i++){
+    let index = i;
+    let element = numbers[index];
+    if(element % 2 === 1){
+      oddNumbers.push(element);
+    }
+  }
+  return(oddNumbers);
+}
+let numbers  = [60, 34, 12, 13, 26, 10, 11];
+
+let oddNumber = getOddNumbersAnArray(numbers);
+let oddNumberSum = getSumNumbers(oddNumber);
+console.log('Odd Number Sum:', oddNumberSum);
