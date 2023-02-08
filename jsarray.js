333cc95e5ddd3be377ff1b47d3ff9c8bf7ec750d
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-02-08 23:46:28 +0600

    array all numbers summation

--- jsarray.js
+++ jsarray.js
@@ -1,2 +1,12 @@
-let numbers  = [2, 12, 45, 60, 34];
-console.log(numbers);
+// array all number summation--------------
+function getSumNumbers(numbers){
+  let sum = 0;
+  for(let i = 0; i < numbers.length; i++){
+    let index = i;
+    let element = numbers[index];
+    sum = sum + element;
+  }
+  // console.log(sum)
+}
+let numbers  = [60, 34, 12, 26, 10];
+getSumNumbers(numbers)
